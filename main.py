6a7b28bc7ac291dcf1f98c76b0c3143c9ce5e61c
vinjn <vinjn.z@gmail.com>
2016-09-06 15:55:28 +0800

    Adding client

--- main.py
+++ main.py
@@ -3,22 +3,58 @@ import time, threading, os
 import win32api
 import VirtualKeyStroke
 
-print "start remote_ppt_controller"
+server_listent_port = 3000
+client_listent_port = 3001
+max_page_number = 30
+
+print '''
+===remote_ppt_controller===
+
+input message types: /page_up, /page_down, /home
+output message types: /page_number + int
+'''
+
+c = None
+s = OSC.OSCServer(('localhost', server_listent_port))
+print s
 
 VirtualKeyStroke.press('F5')
 
-s = OSC.OSCServer(('localhost', 3000))
-print s
+page_number = 0
+
+def send_page_number_back(source):
+    global c
+    if c is None:
+        c = OSC.OSCClient()
+        c.connect((source[0], client_listent_port))
+        print c
+
+    msg = OSCMessage("/page_number")
+    msg.append(page_number)
+    c.send(msg)
 
 # define a message-handler function for the server to call.
 def page_up(addr, tags, data, source):
     VirtualKeyStroke.press('page_up')
+    global page_number
+    page_number -= 1
+    if page_number < 0:
+        page_number = 0
+    send_page_number_back(source)
 
 def page_down(addr, tags, data, source):
     VirtualKeyStroke.press('page_down')
+    global page_number
+    page_number += 1
+    if page_down > max_page_number - 1:
+        max_page_number = max_page_number - 1
+    send_page_number_back(source)
 
 def home(addr, tags, data, source):
     VirtualKeyStroke.press('home')
+    global page_number
+    page_number = 0
+    send_page_number_back(source)
 
 s.addMsgHandler("/page_up", page_up)
 s.addMsgHandler("/page_down", page_down)
